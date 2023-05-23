20fba01d99d85ee0c5783a30fdb422d78a77b6f6
KingVentrix007 <tristanjkuhn007@gmail.com>
2023-05-23 16:06:02 +0200

    Just testing

--- WinLine/console.c
+++ WinLine/console.c
@@ -3,12 +3,13 @@
 #include "string.h";
 #include "console.h";
 #include "erros.h";
+#
 
 
-int* exitCode = 0;
 int main()
 {
-    while (exitCode == 0)
+    int exitValue = 0;
+    while (true)
     {
         char* command[30];
         printf("hello world> ");
