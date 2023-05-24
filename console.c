57cecc234ea7134bb66246573c9a97aaad220973
KingVentrix007 <tristanjkuhn007@gmail.com>
2023-05-24 06:30:29 +0200

    Add if to look for /

--- console.c
+++ console.c
@@ -14,18 +14,37 @@ int main()
     while (exitValue == 0) 
     {
         char* command[30];
-        printf("Enter a command> ");
+        printf("Enter a command3> ");
         fgets(command, sizeof(command), stdin);
-       
-        char * args = strtok(command, "/");
-        if (strcmp(args, "help") == 0)
+        char * args = command;
+        if (strstr(command, " /")!=NULL)
         {
-            printf("Help command");
+            char * argsC = strtok(command, " /");
+            memcpy(args, argsC,strlen(args));
+            printf("/\n");
+
+        }
+        else
+        {
+            char * argsC = command;
+            memcpy(args, argsC,strlen(args));
+            printf("No /\n");
+        }
+        
+        printf("ARGS: %s\n", args);
+        if (strcmp("help",args) == 0)
+        {
+            printf("Help command\n");
         }
         if(strcmp(args, "exit") == 0)
         {
-            printf("Exit");
+            printf("Exit\n");
             exitValue = 1;
+        }
+        else 
+        {
+            printf("Command not found\n");
+
         }
         // while (args != NULL)
         // {
