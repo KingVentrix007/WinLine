68143cb026b7859579de374244805da6fdde6cde
KingVentrix007 <134402045+KingVentrix007@users.noreply.github.com>
2023-06-05 12:57:25 +0200

    add .gitignore file

--- dirent.h
+++ dirent.h
@@ -20,7 +20,7 @@
 
 /*
  * Include windows.h without Windows Sockets 1.1 to prevent conflicts with
- * Windows Sockets 2.0. 
+ * Windows Sockets 2.0.
  */
 #ifndef WIN32_LEAN_AND_MEAN
 #	define WIN32_LEAN_AND_MEAN
